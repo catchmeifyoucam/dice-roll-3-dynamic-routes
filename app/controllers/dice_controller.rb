6d112d674182416937609a8af65d90fb2ed9e358
catchmeifyoucam <camilleruizfunes@gmail.com>
2025-08-17 15:12:51 +0000

    dynamic-routes

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -3,6 +3,22 @@ class DiceController < ApplicationController
     render({ :template => "dice_templates/homepage" })
   end
 
+  #---dice/50/6----
+
+  def roll_flexible
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("number_of_sides").to_i
+
+    @rolls = []
+
+    @num_dice.times do 
+      dice = rand(1..@sides)
+      @rolls.push(dice)
+    end
+    render({ :template => "dice_templates/result_flexible"})
+
+  end
+
   def roll_two_six
     @rolls = []
 
